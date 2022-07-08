0a9ff8485ac0d7e622c7fdca213fb02b26f5015b
Firza Chandra Sandjaya Putra <102524044+FirzaCank@users.noreply.github.com>
2022-07-08 13:40:20 +0700

    Update 8.1 OOP Practice.py

--- IBM Data Engineering Professional/2. Python for Data Scientist, AI, and Dev/8.1 OOP Practice.py
+++ IBM Data Engineering Professional/2. Python for Data Scientist, AI, and Dev/8.1 OOP Practice.py
@@ -34,11 +34,37 @@ class Manusia(object):
     print('{} makan nasi'.format(self.nama))
 
 #manusia kalau belum lahir belum bisa makan, maka lahirkan dulu dengan var programmer baru bisa makan
-programmer = Manusia() #lahirkan dulu - ini adalah buat object
-
-programmer.makan() #fungsi 'makan' baru bisa dipanggil kalau ada objectnya
+programmer = Manusia()      #lahirkan dulu - ini adalah buat object
+programmer.makan()          #fungsi 'makan' baru bisa dipanggil kalau ada objectnya
 
 #output
 None makan nasi
+
+---------------------------------------------------------------------------------------------------------------
+#Diberi nama
+
+class Manusia(object):
+  nama = None
+
+  #fungsi konstruktor
+  def __init__(self,nama): #buat funsi init yang menerima nama
+    self.nama = nama 
+
+  def makan(self):
+    print('{} makan nasi'.format(self.nama))
+
+programmer = Manusia('Eka') 
+programmer.makan()
+
+petani = Manusia('Putra') 
+petani.makan()
+
+dokter = Manusia('Putri') 
+dokter.makan()
+
+#output
+Eka makan nasi
+Putra makan nasi
+Putri makan nasi
 
 ---------------------------------------------------------------------------------------------------------------
