449f5fc30614ba91f5efdc676251135a022ca64f
Firza Chandra Sandjaya Putra <102524044+FirzaCank@users.noreply.github.com>
2022-07-08 13:38:52 +0700

    Create 8.1 OOP Practice.py

---
+++ IBM Data Engineering Professional/2. Python for Data Scientist, AI, and Dev/8.1 OOP Practice.py
@@ -0,0 +1,44 @@
+source: https://www.youtube.com/watch?v=C0p5570Ggng&list=PL3PVN2diT6UPG9DOVzXjmVdaiILJ-hRXg&index=3&t=17s
+
+---------------------------------------------------------------------------------------------------------------
+#Simple function
+
+programmer = 'eka'
+def programmer_makan():
+  print('{} makan nasi'.format(programmer))
+
+petani = 'putra'
+def petani_makan():
+  print('{} makan nasi'.format(petani))
+
+dokter = 'putri'
+def dokter_makan():
+  print('{} makan nasi'.format(dokter))
+
+programmer_makan()
+petani_makan()
+dokter_makan()
+
+#output
+eka makan nasi
+putra makan nasi
+putri makan nasi
+
+---------------------------------------------------------------------------------------------------------------
+#class 1 fungsi dan 1 variabel 'nama'
+
+class Manusia(object):
+  nama = None
+
+  def makan(self):
+    print('{} makan nasi'.format(self.nama))
+
+#manusia kalau belum lahir belum bisa makan, maka lahirkan dulu dengan var programmer baru bisa makan
+programmer = Manusia() #lahirkan dulu - ini adalah buat object
+
+programmer.makan() #fungsi 'makan' baru bisa dipanggil kalau ada objectnya
+
+#output
+None makan nasi
+
+---------------------------------------------------------------------------------------------------------------
